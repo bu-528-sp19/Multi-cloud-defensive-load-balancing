e77a5059aae20d484815c6ee47f91f25e5db38ef
Julian Trinh <julest@bu.edu>
2019-02-07 06:24:35 +0000

    Finished DB CRUD with basic smoke suite

--- test_app/gcp_server/hello_db.go
+++ test_app/gcp_server/hello_db.go
@@ -5,6 +5,7 @@ import (
   "fmt"
   "encoding/json"
   "io/ioutil"
+  "time"
   _ "github.com/lib/pq"
 )
 
@@ -36,6 +37,228 @@ func main() {
 			fmt.Println(reservation.StartTime, reservation.EndTime)
 		}
 	}
+
+	reservationsForUser := getReservationsForUser(testUser.Id)
+	for _, reservation := range reservationsForUser {
+		fmt.Println(reservation.StartTime, reservation.EndTime)
+	}
+
+	newUser := User{Username: "TestUser", Password: "lolhashed", Email: "pls@bu.edu"}
+	newUser = createUser(newUser)
+
+	fmt.Println("User ID: ", newUser.Id)
+
+	newCar := Car{Model: "R8", UserId: newUser.Id}
+	newCar = createCar(newCar)
+
+	newGarage := Garage{Name: "Decent", MaxCars: 7}
+	newGarage = createGarage(newGarage)
+
+	newReservation := Reservation {StartTime: time.Now().String(), EndTime: time.Now().String(), CarId: newCar.Id,GarageId: newGarage.Id}
+	newReservation = createReservation(newReservation)
+
+	deleteReservation(newReservation.Id)
+	deleteGarage(newGarage.Id)
+	deleteCar(newCar.Id)
+	deleteUser(newUser.Id)
+}
+
+func deleteReservation(reservationID int) {
+	db := dbLogin()
+	defer db.Close()
+
+	_, err := db.Query(
+		"DELETE FROM reservations WHERE reservations.id = $1",
+		reservationID)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func createReservation(reservationObj Reservation) Reservation {
+	db := dbLogin()
+	defer db.Close()
+
+	time_layout := "2006-01-02T15:04:05.000Z"
+	start, _ := time.Parse(time_layout, reservationObj.StartTime)
+	end, _ := time.Parse(time_layout, reservationObj.EndTime)
+	row, err := db.Query(
+		"INSERT INTO reservations (start_time, end_time, car_id, garage_id) "+
+		"VALUES($1, $2, $3, $4) "+
+		"RETURNING id",
+		start,
+		end,
+		reservationObj.CarId,
+		reservationObj.GarageId)
+
+	if err != nil {
+		panic(err)
+	}
+
+	row.Next()
+	var newId int
+	scanErr := row.Scan(&newId)
+
+	if scanErr != nil {
+		panic(scanErr)
+	}
+
+	reservationObj.Id = newId
+	return reservationObj
+}
+
+func deleteGarage(garageID int) {
+	db := dbLogin()
+	defer db.Close()
+
+	_, err := db.Query(
+		"DELETE from garages WHERE garages.id = $1",
+		garageID)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func createGarage(garageObj Garage) Garage {
+	db := dbLogin()
+	defer db.Close()
+
+	row, err := db.Query(
+		"INSERT INTO garages (name, max_cars) "+
+		"VALUES($1, $2) "+
+		"RETURNING id",
+		garageObj.Name,
+		garageObj.MaxCars)
+
+	if err != nil {
+		panic (err)
+	}
+
+	row.Next()
+	var newId int
+	scanErr := row.Scan(&newId)
+
+	if scanErr != nil {
+		panic(scanErr)
+	}
+
+	garageObj.Id = newId
+	return garageObj
+}
+
+func deleteCar(carID int) {
+	db := dbLogin()
+	defer db.Close()
+
+	_, err := db.Query(
+		"DELETE FROM cars WHERE cars.id = $1",
+		carID)
+
+	if err != nil {
+		panic (err)
+	}
+}
+
+func createCar(carObj Car) Car {
+	db := dbLogin()
+	defer  db.Close()
+
+	row, err := db.Query(
+		"INSERT INTO cars (user_id, model) "+
+		"VALUES ($1, $2) "+
+		"RETURNING id",
+		carObj.UserId,
+		carObj.Model)
+
+	if err != nil {
+		panic (err)
+	}
+
+	row.Next()
+	var newId int
+	scanErr := row.Scan(&newId)
+
+	if scanErr != nil {
+		panic(scanErr)
+	}
+
+	carObj.Id = newId
+	return carObj
+}
+
+func deleteUser(userID int) {
+	db := dbLogin()
+	defer db.Close()
+
+	_, err := db.Query(
+		"DELETE FROM users where users.id = $1",
+		userID)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func createUser(userObj User) User {
+	db := dbLogin()
+	defer db.Close()
+
+	row, err := db.Query(
+		"INSERT INTO users (username, password, email) "+
+		"VALUES ($1, $2, $3) "+
+		"RETURNING id",
+		userObj.Username,
+		userObj.Password,
+		userObj.Email)
+
+	if err != nil {
+		panic(err)
+	}
+
+	row.Next()
+	var newId int
+	scanErr := row.Scan(&newId)
+
+	if scanErr != nil {
+		panic(scanErr)
+	}
+
+	userObj.Id = newId
+	return userObj
+}
+
+func getReservationsForUser(userID int) []Reservation {
+	db := dbLogin()
+	defer db.Close()
+
+	rows, err := db.Query(
+		"SELECT r.id, r.start_time, r.end_time, r.car_id, r.garage_id "+
+		"FROM reservations r "+
+		"INNER JOIN cars on r.car_id = cars.id "+
+		"INNER JOIN users on users.id = cars.user_id "+
+		"WHERE users.id = $1",
+		userID)
+	var reservations []Reservation
+
+	for rows.Next() {
+		var id int
+		var start string
+		var end string
+		var carID int
+		var garageID int
+
+		err = rows.Scan(&id, &start, &end, &carID, &garageID)
+		if err != nil {
+			panic(err)
+		}
+
+		reservations = append(reservations,
+			Reservation{Id: id, StartTime: start, EndTime: end, CarId: carID, GarageId: garageID})
+	}
+	return reservations
+
 }
 
 func getReservationsForGarage(garageID int) []Reservation {
