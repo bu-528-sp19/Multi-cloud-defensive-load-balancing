7eda5de2dfd7f4a10299920be9a6f6931b30ea4e
rachidtt <rachidtt@bu.edu>
2019-03-24 16:53:57 -0400

    fix 2

--- dao_server/db_access.go
+++ dao_server/db_access.go
@@ -87,10 +87,10 @@ func dbLogin() (*sql.DB) {
 	if (errAWS == nil) && (err!=nil){
 		return dbAWS
 	}
-	else if(errAWS!=nil)&&(err==nil){
+	else if (errAWS!=nil)&&(err==nil){
 		return db
 	}
-	else if(errAWS==nil)&&(err==nil){
+	else if (errAWS==nil)&&(err==nil){
 		rand.Seed(time.Now().UnixNano())
 		dice=rand.Intn(20)
 		if (dice%2 == 0){
