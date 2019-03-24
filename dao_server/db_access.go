6055a4708c3fe21b1621695f87a59d17baea71ec
rachidtt <rachidtt@bu.edu>
2019-03-24 16:08:09 -0400

    added AWS

--- dao_server/db_access.go
+++ dao_server/db_access.go
@@ -5,6 +5,7 @@ import (
   "fmt"
   "os"
   "strconv"
+  "math/rand"
   _ "github.com/lib/pq"
 )
 
@@ -15,52 +16,6 @@ type DatabaseInfo struct {
 	Name     string
 	Port     int
 }
-
-/*func main() {
-	testUser := getUser("jtrinh", "ec528")
-	fmt.Println(testUser.Username, testUser.Email)
-	userCars := getCarsForUser(testUser.ID)
-
-	for _, car := range userCars {
-		fmt.Println(car.Model)
-		reservation := getReservationForCar(car.ID)
-		fmt.Println(reservation.StartTime, reservation.EndTime)
-	}
-
-	garages := getGarages()
-
-	for _, garage := range garages {
-		fmt.Println(garage.Name)
-		reservations := getReservationsForGarage(garage.ID)
-		for _, reservation := range reservations {
-			fmt.Println(reservation.StartTime, reservation.EndTime)
-		}
-	}
-
-	reservationsForUser := getReservationsForUser(testUser.ID)
-	for _, reservation := range reservationsForUser {
-		fmt.Println(reservation.StartTime, reservation.EndTime)
-	}
-
-	newUser := User{Username: "TestUser", Password: "lolhashed", Email: "pls@bu.edu"}
-	newUser = createUser(newUser)
-
-	fmt.Println("User ID: ", newUser.ID)
-
-	newCar := Car{Model: "R8", UserID: newUser.ID}
-	newCar = createCar(newCar)
-
-	newGarage := Garage{Name: "Decent", MaxCars: 7}
-	newGarage = createGarage(newGarage)
-
-	newReservation := Reservation {StartTime: time.Now().String(), EndTime: time.Now().String(), CarID: newCar.ID,GarageID: newGarage.ID}
-	newReservation = createReservation(newReservation)
-
-	deleteReservation(newReservation.ID)
-	deleteGarage(newGarage.ID)
-	deleteCar(newCar.ID)
-	deleteUser(newUser.ID)
-}*/
 
 func testSelect() {
 	db := dbLogin()
@@ -105,5 +60,47 @@ func dbLogin() (*sql.DB) {
 		panic(err)
 	}
 
-	return db
+
+
+//GCP
+
+	port, _ := strconv.Atoi(os.Getenv("PORT"))
+	psqlInfoAWS := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("HOSTAWS"),
+		port,
+		os.Getenv("USER"),
+		os.Getenv("PASSWORDAWS"),
+		os.Getenv("NAME"))
+	dbAWS, errAWS := sql.Open("postgres", psqlInfoAWS)
+
+	if errAWS != nil {
+		panic(errAWS)
+	}
+
+	errAWS = dbAWS.Ping()
+	if errAWS != nil {
+		panic(errAWS)
+	}
+
+
+	if (errAWS == nil) && (err!=nil){
+		return dbAWS
+	}
+	else if(errAWS!=nil)&&(err==nil){
+		return db
+	}
+	else if(errAWS==nil)&&(err==nil){
+		rand.Seed(time.Now().UnixNano())
+		dice=rand.Intn(20)
+		if (dice%2 == 0){
+			return db
+		}
+		else
+			return dbAWS
+
+	}
+
+
+	
 }
