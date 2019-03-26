c1e2a1fceaaf66e06c35f1fa66253a58d69ba63a
joshstern1 <jstern19@bu.edu>
2019-03-26 00:15:15 -0400

    updated db_access

--- dao_server/db_access.go
+++ dao_server/db_access.go
@@ -3,6 +3,10 @@ package main
 import (
   "database/sql"
   "fmt"
+  "os"
+  "strconv"
+  "math/rand"
+  "time"
   _ "github.com/lib/pq"
 )
 
@@ -14,76 +18,9 @@ type DatabaseInfo struct {
 	Port     int
 }
 
-/*func main() {
-	testUser := getUser("jtrinh", "ec528")
-	fmt.Println(testUser.Username, testUser.Email)
-	userCars := getCarsForUser(testUser.ID)
+//deleted test select function
 
-	for _, car := range userCars {
-		fmt.Println(car.Model)
-		reservation := getReservationForCar(car.ID)
-		fmt.Println(reservation.StartTime, reservation.EndTime)
-	}
-
-	garages := getGarages()
-
-	for _, garage := range garages {
-		fmt.Println(garage.Name)
-		reservations := getReservationsForGarage(garage.ID)
-		for _, reservation := range reservations {
-			fmt.Println(reservation.StartTime, reservation.EndTime)
-		}
-	}
-
-	reservationsForUser := getReservationsForUser(testUser.ID)
-	for _, reservation := range reservationsForUser {
-		fmt.Println(reservation.StartTime, reservation.EndTime)
-	}
-
-	newUser := User{Username: "TestUser", Password: "lolhashed", Email: "pls@bu.edu"}
-	newUser = createUser(newUser)
-
-	fmt.Println("User ID: ", newUser.ID)
-
-	newCar := Car{Model: "R8", UserID: newUser.ID}
-	newCar = createCar(newCar)
-
-	newGarage := Garage{Name: "Decent", MaxCars: 7}
-	newGarage = createGarage(newGarage)
-
-	newReservation := Reservation {StartTime: time.Now().String(), EndTime: time.Now().String(), CarID: newCar.ID,GarageID: newGarage.ID}
-	newReservation = createReservation(newReservation)
-
-	deleteReservation(newReservation.ID)
-	deleteGarage(newGarage.ID)
-	deleteCar(newCar.ID)
-	deleteUser(newUser.ID)
-}*/
-
-func testSelect() {
-	db := dbLogin()
-	defer db.Close()
-	rows, err := db.Query("SELECT * FROM users")
-	if err != nil {
-		panic(err)
-	}
-
-	for rows.Next() {
-		var id int
-		var username string
-		var password string
-		var email string
-
-		err = rows.Scan(&id, &username, &password, &email)
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Println(id, username, password, email)
-	}
-}
-
-func dbLogin() (*sql.DB) {
+func dbLoginread() (*sql.DB) {
 	port, _ := strconv.Atoi(os.Getenv("PORT"))
 	psqlInfo := fmt.Sprintf(
 		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
@@ -103,5 +40,68 @@ func dbLogin() (*sql.DB) {
 		panic(err)
 	}
 
-	return db
+
+
+//GCP
+
+	port, _ = strconv.Atoi(os.Getenv("PORT"))
+	psqlInfoAWS := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("HOSTAWS"),
+		port,
+		os.Getenv("USER"),
+		os.Getenv("PASSWORDAWS"),
+		os.Getenv("NAME"))
+	dbAWS, errAWS := sql.Open("postgres", psqlInfoAWS)
+
+	if errAWS != nil {
+		panic(errAWS)
+	}
+
+	errAWS = dbAWS.Ping()
+	if errAWS != nil {
+		panic(errAWS)
+	}
+
+
+	if (errAWS == nil) && (err!=nil){
+		return dbAWS
+	}else if (errAWS!=nil)&&(err==nil){
+		return db
+	}else {
+		rand.Seed(time.Now().UnixNano())
+		dice :=rand.Intn(20)
+		if (dice%2 == 0){
+			return db
+		}else{
+			return dbAWS
+		}
+
+	}
+
+}
+
+func dbLogin() (*sql.DB, *sql.DB) {
+	port, _ := strconv.Atoi(os.Getenv("PORT"))
+	psqlInfo := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("HOST"),
+		port,
+		os.Getenv("USER"),
+		os.Getenv("PASSWORD"),
+		os.Getenv("NAME"))
+	//db, err := sql.Open("postgres", psqlInfo)//err declared and not used
+	db, _ := sql.Open("postgres", psqlInfo)
+	port, _ = strconv.Atoi(os.Getenv("PORT"))
+	psqlInfoAWS := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("HOSTAWS"),
+		port,
+		os.Getenv("USER"),
+		os.Getenv("PASSWORDAWS"),
+		os.Getenv("NAME"))
+	//dbAWS, errAWS := sql.Open("postgres", psqlInfoAWS)//errAWs declared and not used
+	dbAWS, _ := sql.Open("postgres", psqlInfoAWS)
+
+	return db,dbAWS
 }
